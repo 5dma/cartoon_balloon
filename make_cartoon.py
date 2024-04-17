6f740c4d2a9c9ed31fe29ba89261fadde7d1d7d1
5dma <mark@lautman.net>
2024-04-17 17:18:02 -0400

    Second commit

--- make_cartoon.py
+++ make_cartoon.py
@@ -2,6 +2,7 @@ from wand.image import Image
 from wand.drawing import Drawing
 import argparse
 from os.path import exists
+import json
 
 parser = argparse.ArgumentParser(description='Creates a balloon of text at a specified point in an image')
 parser.add_argument('-i','--image',action='store',required=True, help='Source image')
@@ -13,36 +14,51 @@ if not exists(args.image):
 	exit()
 
 image1 = Image(filename=args.image)
+text_string = args.text
 
 # Scale image to 520 width
 dimensions = image1.size
-newheight = int((520/dimensions[0] ) * dimensions[1])
-image1.resize(520, newheight)
+new_width = 520
+new_height = int((new_width/dimensions[0] ) * dimensions[1])
+image1.resize(new_width, new_height)
 image1.save(filename="stage_1.jpg")
+
+balloon_fill_color = '#FEFEFE'
+balloon_stroke_color = '#000000'
 
 # Add text
 text = Drawing()
 text.font = 'Verdana'
-text.fill_color = '#000000'
-text.font_size = 40
-text.text(150,150,'OMGBARF')
-text(image1)
+text.fill_color = balloon_stroke_color
+text.font_size = 20
+text.gravity = 'north_west'
+text.text(120,105,text_string)
+metrics = text.get_font_metrics(image1,text_string)
+
+(text_width, text_height) = metrics.size()
+print("text height: {0}, text width: {1}".format(text_height, text_width))
 image1.save(filename="stage_2.jpg")
 
 # Add balloon
+rectangle_height = text_height
+rectangle_width = text_width + 50
 balloon = Drawing()
-balloon.fill_color = '#FF0000'
-balloon.rectangle(20,20,100,100)
-balloon(image1)
-image1.save(filename="stage_3.jpg")
+balloon.fill_color = balloon_fill_color
+balloon.stroke_color = balloon_stroke_color
+balloon.rectangle(120,105, None, None, rectangle_width, rectangle_height)
+
 
 
 #Add path
 points = [(100,100),(200,200),(300,100)]
 path = Drawing()
-path.fill_color = '#00FF00'
-path.stoke_color = '#000000'
+path.fill_color = balloon_fill_color
+path.stoke_color = balloon_stroke_color
 path.polyline(points)
-path(image1)
 image1.save(filename="stage_4.jpg")
 
+#Assemble image
+balloon(image1)
+path(image1)
+text(image1)
+image1.save(filename="stage_3.jpg")
